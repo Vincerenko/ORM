9017241c8c0fefb3aa5f6af95b8a5b89068317bb
san <vencer97shoes@gmail.com>
2021-07-30 10:58:26 +0200

    added logging

--- src/main/java/utils/PostgreSqlTypeConverter.java
+++ src/main/java/utils/PostgreSqlTypeConverter.java
@@ -6,11 +6,11 @@ import java.util.Map;
 public class PostgreSqlTypeConverter implements SqlTypeConverter{
     public final Map<Class<?>, String> CONVERTER = new HashMap<Class<?>, String>() {
         {
-            put(Integer.class, "INT");//
-            put(Long.class, "BIGINT");//
-            put(Double.class, "INT");//
-            put(Float.class, "INT");//
-            put(Short.class, "INT");//
+            put(Integer.class, "INT");
+            put(Long.class, "BIGINT");
+            put(Double.class, "INT");
+            put(Float.class, "INT");
+            put(Short.class, "INT");
             put(Byte.class, "INT");
             put(Boolean.class, "VARCHAR (6)");
             put(Character.class, "VARCHAR (250)");
